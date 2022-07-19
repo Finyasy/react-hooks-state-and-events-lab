f333b3f1a3ca62b2ee6fbec1e7bd026665203419
Bryan Bosire <bryanbosire@gmail.com>
2022-07-19 05:57:31 +0300

    commit tree 1e9eff90fadc065372e42f0a954225efa9c795a7

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React,{useState} from "react";
 
 function Item({ name, category }) {
+  const [isInCat, setIsInCat] = useState(false);
+
+  function handleAddToCat() {
+    setIsInCat((isInCat) => !isInCat);
+  }
+
   return (
-    <li className="">
+    <li className={isInCat? "in-cart":""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={isInCat ? "remove":"add"}
+        onClick={handleAddToCat}
+      >
+        {isInCat ? "Remove From" : "Add To"} Cart
+      </button>
     </li>
   );
 }
